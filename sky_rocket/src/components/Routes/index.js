504e4bb11cebbb2ab134858a0cf9f5626b28e3e5
lionelcoding <louis.lionel@gmail.com>
2022-03-15 19:40:04 +0100

    SignUp,SignIn and JWT work from front

--- sky_rocket/src/components/Routes/index.js
+++ sky_rocket/src/components/Routes/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-import Home from '../../pages/Home';
-import Profil from '../Profil/Profil';
+import Home from "../../pages/Home";
+import Profil from "../../pages/Profil";
 
 const index = () => {
-    return (
+  return (
     <div>
-             <BrowserRouter>
-
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/profil" element={<Profil />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profil" element={<Profil />} />
+        </Routes>
       </BrowserRouter>
     </div>
-
-    );
+  );
 };
 
 export default index;
