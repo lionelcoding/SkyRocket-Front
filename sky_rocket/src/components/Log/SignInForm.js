73941b7e6bd04ae9da67f0c2a83e888bf565c40c
lionelcoding <louis.lionel@gmail.com>
2022-03-15 12:49:48 +0100

    set up router, component

---
+++ sky_rocket/src/components/Log/SignInForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignInForm = () => {
+    return (
+        <div>
+            SignIn
+        </div>
+    );
+};
+
+export default SignInForm;
---
+++ sky_rocket/src/components/Log/SignUpForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpForm = () => {
+    return (
+        <div>
+            SignUP
+        </div>
+    );
+};
+
+export default SignUpForm;
