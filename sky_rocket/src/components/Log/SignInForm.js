4a6228f2e1ac1d9e1d66613c5545c15e728b2dfb
lionelcoding <louis.lionel@gmail.com>
2022-03-19 15:22:02 +0100

    redux with issues

--- sky_rocket/src/components/Log/SignInForm.js
+++ sky_rocket/src/components/Log/SignInForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
+// import  from "js-cookie";
 
 const SignInForm = () => {
   const [email, setEmail] = useState("");
@@ -30,7 +30,7 @@ const SignInForm = () => {
           passwordError.innerHTML = res.data.errors.password;
           return 
         }
-        Cookies.set('jwt', res.data.jwt)
+        // Cookies.set('jwt', res.data.jwt);
         navigate('/', {replace: true}) ;
       
       })
--- sky_rocket/src/index.js
+++ sky_rocket/src/index.js
@@ -2,19 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import rootReducer from "./reducers";
+
+// //redux
+// import { createStore } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import thunk from "redux-thunk";
+// import { Provider } from "react-redux";
+// import { applyMiddleware } from "redux";
+// import { getPosts } from "./actions/post.action";
 
 // const store = createStore(
 //   rootReducer,
 //   composeWithDevTools(applyMiddleware(thunk))
 // );
 
-// store.dispatch(getPosts());
+// store.dispatch(getPosts())
 
 ReactDOM.render(
 
-
-    <App />,
-
+    <App />
+,
   document.getElementById("root")
 );
