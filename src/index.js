36ecea2d0946f84233eac2848dc91ed5f39107f3
Ju4n-P <th3dr34mm4k3r@gmail.com>
2022-03-22 11:58:10 +0100

    display all post in home

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+// dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+import { getPosts } from "./actions/post.actions";
 
-// const store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(thunk))
-// );
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
-// store.dispatch(getPosts());
+store.dispatch(getPosts());
 
 ReactDOM.render(
-    <App />,
+  <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+</React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Thread from "../components/Thread";
 
 
 const Home = () => {
     return (
         <div className="container p-20 ">
+          <Thread/>
           Home
         </div>
     );
