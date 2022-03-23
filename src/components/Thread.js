dacf684687e07ea3dd92673912eedbbbc34027f6
Ju4n-P <th3dr34mm4k3r@gmail.com>
2022-03-23 12:54:44 +0100

    add style for card 02

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
-import {useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Card from "./Card/Card";
 import { isEmpty } from "./Utils";
 
 const Thread = () => {
-
   const posts = useSelector((state) => state.postReducer);
 
-
-
-
   return (
-    <div className="thread-container">
-      <>
-        {!isEmpty(posts[0]) &&
-          posts.map((post) => {
-            return <Card post={post} key={post._id}/>;
-          })}
-      </>
+    <div class="container px-4 py-5" id="custom-cards">
+      <h2 class="pb-2 border-bottom">{}</h2>
+      <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+
+      {!isEmpty(posts[0]) &&
+        posts.map((post) => {
+          return <Card post={post} key={post._id} />;
+        })}
+        </div>
     </div>
   );
 };
