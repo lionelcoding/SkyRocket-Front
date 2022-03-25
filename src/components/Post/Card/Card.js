1894d38c252e3cd350fef0c8ff4496e588975d52
lionelcoding <louis.lionel@gmail.com>
2022-03-25 15:19:08 +0100

    try to implement design in card with react-bootstrap

--- src/components/Post/Card/Card.js
+++ src/components/Post/Card/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { isEmpty } from "../../Utils";
 
@@ -6,33 +7,48 @@ const Card = ({ post }) => {
   const usersData = useSelector((state) => state.usersReducer);
 
   return (
-    <div class="card">
-      <img
-        class="card-img-top"
-        src="https://source.unsplash.com/1600x900/?startup,meeting"
-        alt="Card image cap"
-      />
-      <div class="card-body">
-        <p class="card-text">
-          <ul>
-            <li>{post.message}</li>
-            <li class="mt-10">crée le {post.createdAt}</li>
-            <li class="mt-5">Participants : {post.likers.length}</li>
-            <li>
-              posté par{" "}
-              {!isEmpty(usersData[0]) &&
-                usersData
-                  .map((user) => {
-                    if (user._id === post.posterId) return user.pseudo;
-                    else return null;
-                  })
-                  .join("")}
-            </li>
-          </ul>
-        </p>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <img
+          class="card-img-top"
+          src="https://source.unsplash.com/1600x900/?startup,meeting"
+          alt="Card image cap"
+        />
+      </Row>
+      <Row>{post.message}</Row>
+      <Row>
+        {" "}
+        <Col>{post.createdAt}</Col>
+        <Col>Participants : {post.likers.length}</Col>
+      </Row>
+      posté par{" "}
+      {!isEmpty(usersData[0]) &&
+        usersData
+          .map((user) => {
+            if (user._id === post.posterId) return user.pseudo;
+            else return null;
+          })
+          .join("")}
+    </Container>
   );
 };
 
 export default Card;
+
+{
+  /* <CardGroup>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>; */
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,14 +2,14 @@ import React, { useContext } from "react";
 import Thread from "../../components/Thread/Thread";
 import { UidContext } from "../../components/AppContext";
 import BackgroundVideo from "../../components/BackgroundVideo/BackgroundVideo";
-import "./Home.scss"
+import "./Home.scss";
 const Home = () => {
   const uid = useContext(UidContext);
   return (
-    <div >
-      <BackgroundVideo />
+    <div>
+      {/* <BackgroundVideo /> */}
       <div className="ThreadHome">
-      <Thread />
+        <Thread />
       </div>
     </div>
   );
