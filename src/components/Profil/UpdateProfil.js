f129c16bd04a70caae34e81e801b8ff5e4211011
lionelcoding <louis.lionel@gmail.com>
2022-03-25 12:11:10 +0100

    styling bio

--- src/components/Profil/UpdateProfil.js
+++ src/components/Profil/UpdateProfil.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { updateBio } from "../../actions/user.actions";
 import userReducer from "../../reducers/user.reducer";
@@ -20,38 +21,53 @@ const UpdateProfil = () => {
   };
 
   return (
-    <div>
-      <h1> Profil de {userData.pseudo}</h1>
-      <div>
-        <div>
-          <UploadImg />
+    <Container>
+      <Row>
+        <Col>
+          <h1> Profil de {userData.pseudo}</h1>
+        </Col>
+        <Col>
+          <h3>Bio</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <img src={userData.picture} alt="user-pic" />
-        </div>
-        <div>
-          <div>
-            <h3>Bio</h3>
-            {updateForm === false && (
-              <>
-                <p onClick={() => setUpdateForm(!updateForm)}>{userData.bio}</p>
-                <button onClick={() => setUpdateForm(!updateForm)}>
-                  Modifier bio
-                </button>
-              </>
-            )}
-            {updateForm && (
-              <>
-                <textarea
-                  type="text"
-                  defaultValue={userData.bio}
-                  onChange={(e) => setBio(e.target.value)}
-                ></textarea>
-                <button onClick={handleUpdate}>Valider modifications</button>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
+        </Col>
+        <Col>
+          {updateForm === false && (
+            <>
+              <p onClick={() => setUpdateForm(!updateForm)}>{userData.bio}</p>
+              <Button onClick={() => setUpdateForm(!updateForm)}>
+                Modifier bio
+              </Button>
+            </>
+          )}
+          {updateForm && (
+            <>
+              <Form>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>
+                    Expliquez nous en quelques mots ce qui vous rend unique{" "}
+                  </Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    type="text"
+                    defaultValue={userData.bio}
+                    onChange={(e) => setBio(e.target.value)}
+                  />
+                </Form.Group>
+                <Button onClick={handleUpdate}>Valider modifications</Button>
+              </Form>
+            </>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
